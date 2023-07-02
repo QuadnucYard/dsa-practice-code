e102117a26ffe29cc9301e367a01e83e468cf22f
QuadnucYard <2380433991@qq.com>
2023-07-02 09:48:01 +0800

    Organize files

--- src/proj2/test_sort.cpp
+++ src/proj2/test_sort.cpp
@@ -1,7 +1,8 @@
 #define DEBUG
 #include "external_quick_sort.hpp"
 int main() {
-	external_quick_sorter<int> sorter(1 << 10);
+	external_quick_sorter<int> sorter(100000);
+	fs::current_path("E:/Project/dsa-practice-code/proj2");
 	sorter("data/arr.in", "data/arr.out");
 	return 0;
 }
